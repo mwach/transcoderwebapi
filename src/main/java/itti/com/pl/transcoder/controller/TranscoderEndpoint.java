6dfd2b72643fdcc816e586251b6a811c7a3316e7
mwach <marcin.wachowiak@gmail.com>
2015-08-06 22:38:59 +0200

    ws and xjc support

--- src/main/java/itti/com/pl/transcoder/controller/TranscoderEndpoint.java
+++ src/main/java/itti/com/pl/transcoder/controller/TranscoderEndpoint.java
@@ -1,20 +1,37 @@
 package itti.com.pl.transcoder.controller;
 
-import java.util.UUID;
-
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import itti.com.pl.transcoder.dto.Bitrate;
+import itti.com.pl.transcoder.dto.Configuration;
+import itti.com.pl.transcoder.dto.GetConfigurationResponse;
+import itti.com.pl.transcoder.dto.SetConfigurationRequest;
+import itti.com.pl.transcoder.dto.Size;
 
 @Endpoint
 public class TranscoderEndpoint {
 
-	private static final String NAMESPACE_URI  = "itti.com.pl.transcoderwebapi";
+	private static final String NAMESPACE_URI  = "http://dto.transcoder.pl.com.itti";
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart="getConfiguration")
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart="getConfigurationRequest")
 	@ResponsePayload
-	public String getConfiguration(){
-		return UUID.randomUUID().toString();
+	public GetConfigurationResponse getConfiguration(){
+		GetConfigurationResponse response = new GetConfigurationResponse();
+		Configuration c = new Configuration();
+		c.setBitrate(Bitrate.EUR);
+		c.setFps(34);
+		c.setSize(Size.HD);
+		response.setConfiguration(c);
+		return response;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart="setConfigurationRequest")
+	public void setConfiguration(@RequestPayload SetConfigurationRequest request){
+		request.setConfiguration(new Configuration());
+		request = null;;
 	}
 
 }
